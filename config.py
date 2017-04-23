a341d21ac1046eb1215eddcfc32daba80aab27b4
dgamliel <dgamliel7@gmail.com>
2017-04-23 14:49:09 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+ername = "emojifyBot"
+password = "Dargasw4h8f6"
+client_id = "I_zxumbp68K31w"
+client_secret = "e_VxuJ9TXgu0z6ve6XpSmOZSZyg"
---
+++ emojiBot.py
@@ -0,0 +1,37 @@
+import praw
+import time
+import config
+import requests
+
+
+trigger = "!emojify"
+
+def login(r):
+
+	reddit = praw.Reddit(username = config.username,
+ 		     	    password = config.password,
+		     	    client_id = config.client_id,
+		     	    client_secret = config.client_secret)
+
+	return reddit
+
+
+def writeToFile(commentID):
+	infile = open('idFile.txt', 'a+')
+	idList = infile.readlines()
+	if commentID not in idList:
+		infile.write(commentID + "\n")
+		infile.close()
+
+def verifyConnection(request):
+	print(requests.url())
+	print()
+
+def emojify():
+	req = requests.get("emojipasta.co")
+
+
+def botGetComment(trigger):
+	for comment in reddit.subreddit('all').comments(limit=50):
+	if comment == trigger and comment.id not in idList:
+		writeToFile(comment.id)
