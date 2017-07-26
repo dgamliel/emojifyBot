f4aeb5806a887b50f71093873b06a50d57100fed
dgamliel <dgamliel@umail.ucsb.edu>
2017-07-25 19:45:22 -0700

    patched items

--- emojiBot.py
+++ emojiBot.py
@@ -5,16 +5,16 @@ import requests
 import bs4
 
 '''
-This comment is the "trigger"
+This strings is the "trigger"
 that when found in a reddit comment
 will prompt our script to reply to the comment
 with the emojified text of the parent comment.
 '''
 
-trigger = "!emojify"
+phrase = "!emojify"
 
 
-def login(r):
+def login():
 
 	'''
 	Initializes a reddit instance
@@ -26,11 +26,14 @@ def login(r):
 	reddit = praw.Reddit(username = config.username,
  		     	    password = config.password,
 		     	    client_id = config.client_id,
-		     	    client_secret = config.client_secret)
+		     	    client_secret = config.client_secret,
+					user_agent = config.user_agent)
 
-	return reddit #Returns a reddit object, essentially "logs in"
+	print ("Successfully logged into reddit")
 
+	return reddit #Returns a reddit instance, essentially "logs in"
 
+#Writes to 'idFile.txt' to make sure we only respond to a comment once
 def writeToFile(commentID):
 
 	#Opens a file called "idFile.txt"
@@ -55,29 +58,54 @@ def verifyConnection(request):
 	print(requests.url())
 
 
-#Pretty sure this parses through all the comments in a subreddit
-#and if the trigger string is found, then the script writes to file
-#the comment ID
 
-def botGetComment(trigger):
-	#putting 'test' as reddit.subreddit parameter so that
-	#I can properly test the bot before "deploying"
-	for comment in reddit.subreddit('test').comments(limit=50):
-		if comment == trigger and comment.id not in idList:
+#Returns parent comment of comment that triggered bot response
+def getComment(trigger):
+
+	#Generates a list of ID's to parse through
+	#TODO: Make this a hashbank/Dict somehow?
+	idFile = open('idFile.txt', 'r')
+	idList = idFile.readlines()
+	idFile.close()
+
+	#Parses comments and returns the parent of trigger comment
+	#TODO: fetch and return parent comment
+	for comment in reddit.subreddit('test').comments(limit=75):
+		if comment == trigger and comment.id not in idList and not comment.is_root():
+			
+			print('FOUND MATCHING COMMENT')
 			writeToFile(comment.id)
+			parent = comment.parent()
+
+			print(parent.body())
+			return parent.body()
+
+		else:
+			print('COMMENT NOT FOUND')
 
 def scrape(comment):
 	textBox = "<textarea class='form-control animated' rows='6' id='text' accept-charset='utf-8'></textarea>"
 
 	#sends a get requests to emojipasta.co and receives index.html data
 	req = requests.get("http://emojipasta.co")
+	print ("GET request sent to : http://emojipasta.co")
 	
 	#intializes a BS4 object passing the HTML of the text box from the emojipasta website
-	soup = BeautifulSoup(textBox)
+	#Uses 'lxml' to parse (I don't know what this means)
+	soup = bs4.BeautifulSoup(textBox, "lxml")
 
 	#inserts our comment into the box?
-	soup.insert(comment)
+	soup.textarea.append(comment)
+	print ("Soup object now looks like : ")
+	print (soup)
 
-	#TODO: figure out how the fuck server requests work. 
+	#TODO: figure out how the fuck server requests work.
 
-	
+#--------------------------------------------------------
+
+while True:
+	reddit = login()
+	copyPasta = getComment(phrase)
+	scrape(copyPasta)
+	print ('NOW SLEEPING FOR 10000 UNITS')
+	time.sleep(10000)
